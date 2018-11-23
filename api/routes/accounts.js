015fd620e83cc2ba0985553c1dd3293a6dd32cbd
Nicolas AUGER <nicolas@poool.fr>
2018-11-23 20:50:10 +0100

    feat: account routes

--- api/routes/accounts.js
+++ api/routes/accounts.js
@@ -1,12 +1,41 @@
 const express = require('express');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 const Account = require('../models/Account');
 
-router.post('/', (req, res) => {
+router.get('/', async (req, res, next) => {
+    const limit = parseInt(req.query.count) || 10;
+    const offset = parseInt(req.query.offset) || 0;
+    const search = req.query.search || false;
+    const conditions = {};
+
+    const accounts = await Account
+        .find(conditions)
+        .skip(offset)
+        .limit(limit)
+        .sort({ createdAt: -1 }).then(accounts => {
+            res.status(200).json({
+                message: 'Accounts fetched successfully',
+                accounts: accounts
+            });
+        }).catch(err => {
+            res.status(500).json({ message: err.message });
+        });
+});
+
+router.post('/', async (req, res) => {
+    const existingAccount = await Account.findOne({ email: req.body.email });
+    if (existingAccount) {
+        res.status(400).json({
+            error: 'BadRequest',
+            message: 'An account already exists with this email.'
+        });
+    }
     const account = new Account({
-        name: req.body.name,
-        pseudo: req.body.pseudo,
+        fullname: req.body.fullname,
+        email: req.body.email,
         password: req.body.password,
         address: req.body.address,
         city: req.body.city,
@@ -15,14 +44,36 @@ router.post('/', (req, res) => {
     account
         .save()
         .then(result => {
-            console.log(result);
-        })
-        .catch (err => console.log(err));
-    res.status(200).json({
-        message: 'Handling POST requests to /accounts',
-        account: account
-    });
+            res.status(200).json({ success: true, account: result });
+        }).catch(err => {
+            res.status(500).json({ message: err.message });
+        });
 });
+
+router.post('/login', async (req, res) => {
+    const account = await Account.findOne({ email: req.body.email });
+    if (!account) {
+        res.status(400).json({
+            error: 'BadRequest',
+            message: 'No account found for provided email.'
+        });
+    } else {
+        console.log('account : ', account);
+        if (account.password !== crypto.createHmac("sha256", account.salt).update(req.body.password).digest("hex")) {
+            res.status(400).json({
+                error: 'BadRequest',
+                message: 'Provided password is not valid.'
+            });
+        }
+        const token = jwt.sign({
+            email: account.email,
+            userId: account._id
+        }, process.env.JWT_KEY, { expiresIn: '1h' });
+        res.status(200).json({ success: true, account: account, token: token });
+    }
+
+
+})
 
 
 module.exports = router;
