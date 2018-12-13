a976b53ee839b164a42a355912603667c35b3499
Nicolas AUGER <nicolas@poool.fr>
2018-12-13 12:02:29 +0100

    feat: GET / DEL starters endpoint

--- api/routes/starters.js
+++ api/routes/starters.js
@@ -79,13 +79,72 @@ router.get('/:id', checkAuth, (req, res, next) => {
                 message: 'The ressource you are looking for is forbidden.'
             });
         } else {
-            if (utils.requestIsEmpty(req.params.id)) {
-            if (utils.requestIsEmpty(req.headers.account)) {
-                res.status(400).json({ message: 'You must provide an account id in your request headers.' });
+            if (utils.requestIsEmpty(req.params.id) || !ObjectId.isValid(req.params.id)) {
+                res.status(400).json({ message: 'Cannot get starter, empty request.' });
             }
             if (utils.requestIsEmpty(req.headers.account)) {
                 res.status(400).json({ message: 'You must provide an account id in your request headers.' });
             }
+            const starter = Starter
+                .findOne({
+                    _id: req.params.id,
+                    account: req.headers.account
+                })
+                .then(starter => {
+                    res.status(200).json({
+                        message: 'Starter fetched successfully',
+                        starter: starter
+                    });
+                }).catch(err => {
+                    res.status(500).json({ message: err.message });
+                });
+        }
+    });
+});
+
+router.post('/:id', checkAuth, (req, res, next) => {
+    jwt.verify(req.token, process.env.JWT_KEY, async function(err, data) {
+        if (err) {
+            res.status(403).json({
+                error: 'Forbidden',
+                message: 'The ressource you are looking for is forbidden.'
+            });
+        } else {
+            if (utils.requestIsEmpty(req.params.id) || !ObjectId.isValid(req.params.id)) {
+                res.status(400).json({ message: 'Cannot update starter, empty request.' });
+            }
+            if (utils.requestIsEmpty(req.headers.account)) {
+                res.status(400).json({ message: 'You must provide an account id in your request headers.' });
+            }
+            const starter = await Starter.findOne({
+                _id: req.params.id,
+                account: req.headers.account
+            });
+
+            Object.assign(starter, req.body);
+
+            starter
+                .save()
+                .then(result => {
+                    res.status(200).json({
+                        message: 'Starter updated with success.',
+                        starter: starter
+                    });
+                }).catch(err => {
+                    res.status(500).json({ message: err.message });
+                });
+        }
+    });
+});
+
+router.delete('/:id', checkAuth, (req, res, next) => {
+    jwt.verify(req.token, process.env.JWT_KEY, function(err, data) {
+        if (err) {
+            console.log(err);
+            res.status(403).json({
+                error: 'Forbidden',
+                message: 'The ressource you are looking for is forbidden.'
+            });
         } else {
             if (utils.requestIsEmpty(req.params.id) || !ObjectId.isValid(req.params.id)) {
                 res.status(400).json({ message: 'Cannot delete starter, empty request.' });
