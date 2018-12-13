8ff5b8aa8213a1fd97beadccdcf2387a5ead705e
Nicolas AUGER <nicolas@poool.fr>
2018-12-13 12:10:12 +0100

    refactor: rework orders endpoint

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -14,12 +14,15 @@ router.get('/', checkAuth, (req, res, next) => {
                 message: 'The ressource you are looking for is forbidden.'
             });
         } else {
+            if (utils.requestIsEmpty(req.headers.account)) {
+                res.status(500).json({ message: 'You must provide an account id in your request headers.' });
+            }
+
             const limit = parseInt(req.query.count) || 10;
             const offset = parseInt(req.query.offset) || 0;
             const search = req.query.search || false;
-            const conditions = {};
             const orders = await Order
-                .find(conditions)
+                .find({ account: req.headers.account })
                 .skip(offset)
                 .limit(limit)
                 .sort({ createdAt: 1 }).then(orders => {
@@ -34,47 +37,41 @@ router.get('/', checkAuth, (req, res, next) => {
     });
 });
 
-router.post('/', checkAuth, (req, res, next) => {
-    jwt.verify(req.token, process.env.JWT_KEY, async function(err, data) {
-        if (err) {
-            res.status(403).json({
-                error: 'Forbidden',
-                message: 'The ressource you are looking for is forbidden.'
-            });
-        } else {
-            if (utils.requestIsEmpty(req.body)) {
-                res.status(400).json({ message: 'Cannot create order, empty request.' });
-            }
-            let lastOrder = await Order
-                .find({ account: req.body.account })
-                .limit(1)
-                .sort({ createdAt: -1 });
+router.post('/', async (req, res, next) => {
+    if (utils.requestIsEmpty(req.body)) {
+        res.status(400).json({ message: 'Cannot create order, empty request.' });
+    }
+    if (utils.requestIsEmpty(req.headers.account)) {
+        res.status(500).json({ message: 'You must provide an account id in your request headers.' });
+    }
+    let lastOrder = await Order
+        .find({ account: req.body.account })
+        .limit(1)
+        .sort({ createdAt: -1 });
 
-            const order = new Order({
-                account: req.body.account,
-                orderId: lastOrder.length > 0 ? lastOrder[0].orderId + 1 : 0,
-                menus: req.body.menus,
-                starters: req.body.starters,
-                mainCourses: req.body.mainCourses,
-                desserts: req.body.desserts,
-                supplements: req.body.supplements,
-                table: req.body.table,
-                served: false,
-                totalPrice: req.body.totalPrice
-            });
+    const order = new Order({
+        account: req.headers.account,
+        orderId: lastOrder.length > 0 ? lastOrder[0].orderId + 1 : 0,
+        menus: req.body.menus,
+        starters: req.body.starters,
+        mainCourses: req.body.mainCourses,
+        desserts: req.body.desserts,
+        supplements: req.body.supplements,
+        table: req.body.table,
+        served: false,
+        totalPrice: req.body.totalPrice
+    });
 
-            order
-                .save()
-                .then(result => {
-                    res.status(200).json({
-                        message: 'New order created with success.',
-                        order: order
-                    });
-                }).catch(err => {
-                    res.status(500).json({ message: err.message });
-                });
-        }
-    })
+    order
+        .save()
+        .then(result => {
+            res.status(200).json({
+                message: 'New order created with success.',
+                order: order
+            });
+        }).catch(err => {
+            res.status(500).json({ message: err.message });
+        });
 });
 
 router.delete('/:id', (req, res, next) => {
@@ -85,12 +82,16 @@ router.delete('/:id', (req, res, next) => {
                 message: 'The ressource you are looking for is forbidden.'
             });
         } else {
-            if (utils.requestIsEmpty(req.params.id)) {
+            if (utils.requestIsEmpty(req.params.id) || !ObjectId.isValid(req.params.id)) {
                 res.status(400).json({ message: 'Cannot delete order, empty request.' });
+            }
+            if (utils.requestIsEmpty(req.headers.account)) {
+                res.status(500).json({ message: 'You must provide an account id in your request headers.' });
             }
             const id = req.params.id;
             Order.deleteOne({
                 _id: req.params.id
+                account: req.headers.account
             }).then(result => {
                 res.status(200).json({
                     success: true,
